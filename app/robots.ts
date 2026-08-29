1d1e6469c0c15b84f65d7b104b03fa3d79e8a093
seacollab43 <lahlouabderrahmane40@gmail.com>
2026-08-29 23:38:33 +0100

    feat: add app/robots.ts for Vet24

---
+++ app/robots.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      { userAgent: "*", allow: "/" },
+      { userAgent: "GPTBot", allow: "/" },
+      { userAgent: "PerplexityBot", allow: "/" },
+      { userAgent: "ClaudeBot", allow: "/" },
+    ],
+    sitemap:
+      "https://site-vitrine-abderrahmane.nq88h8ydw2.chatgpt.site/sitemap.xml",
+  };
+}
